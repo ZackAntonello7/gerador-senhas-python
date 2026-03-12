1ce5759c54966a47c41342b8fba8fefc710865d5
ZackAntonello7 <thierry.pengo@sou.unifeob.edu.br>
2026-03-11 22:00:18 -0300

    Add files via upload

---
+++ gsenhas.py
@@ -0,0 +1,14 @@
+import random
+import string
+
+print("=== GERADOR DE SENHAS ===")
+
+tamanho = int(input("Digite o tamanho da senha: "))
+
+caracteres = string.ascii_letters + string.digits + string.punctuation
+
+senha = ""
+for i in range(tamanho):
+    senha += random.choice(caracteres)
+
+print("Senha gerada:", senha)
